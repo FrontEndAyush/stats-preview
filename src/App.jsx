f9436b3c30033322c349b549d57c250b2e1a822f
ayush <iamrealglider@gmail.com>
2024-07-31 23:56:18 +0530

    made some fucntionality

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <>
+      <main className="flex items-center  justify-center h-screen">
+        <div className="flex container  ">
+          <div className="w-6/12 text-white mt-8">
+            <h1 className="text-white font-fam text-3xl font-bold text-center ">
+              Get <span className="text-[#A855D3] ">insights</span> that help
+            </h1>
+            <h1 className="text-center text-3xl font-bold ">
+              your business grow.
+            </h1>
+          </div>
+
+          <div className="w-6/12">
+            <img src="/images/image-header-desktop.jpg" alt="" />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
